40e6306de75679e6b1e6c16ea07e0c19613aa497
40030361 <jyoung22@qub.ac.uk>
2021-04-20 20:21:59 +0100

    Removes files which have been made redundant by new Controller class

--- api/index.php
+++ api/index.php
@@ -15,17 +15,7 @@ include_once '../autoload.php';
 //use helpers\Database;
 use models\{APIAccess, Athlete, Event, Fight, User};
 use controllers\Controller;
-use controllers\EventController;
-use \controllers\FightController;
 use \helpers\Database;
-
-// http headers
-const HTTP_SUCCESS = 'HTTP/1.1 200 OK';
-const HTTP_CREATED = 'HTTP/1.1 201 Created';
-const HTTP_SUCCESS_NO_CONTENT = 'HTTP/1.1 204 No Content'; // update and delete
-const HTTP_UNAUTHORIZED = 'HTTP/1.1 401 Unauthorized';
-const HTTP_NOT_FOUND = 'HTTP/1.1 404 Not Found';
-const HTTP_FORBIDDEN = 'HTTP/1.1 403 Forbidden';
 
 // api endpoint modules
 const FIGHT_MODULE = 'fight';
@@ -59,36 +49,33 @@ $id = (!empty($id) ? intval($id) : null);
 $api_access = new APIAccess($db);
 
 if (!$api_access->verifyKey($api_key)) {
-    header(HTTP_UNAUTHORIZED);
+    header(Controller::HTTP_UNAUTHORIZED);
     exit();
 }
 
 // get user
 $user = new User($db);
 if (!$user->getByUserId($api_access->getUserId())) {
-    header(HTTP_NOT_FOUND);
+    header(CONTROLLER::HTTP_NOT_FOUND);
     echo json_encode(['error' => 'User could not be found']);
     exit();
 }
 
 switch ($api_module) {
     case FIGHT_MODULE:
-//        $fight = new FightController($db, $requestMethod, $id, $user);
         $fight = new Controller(new Fight($db), $user, $requestMethod, $id);
         $fight->process_request();
         break;
     case EVENT_MODULE:
-//        $event = new EventController($db, $requestMethod, $id, $user);
         $event = new Controller(new Event($db), $user, $requestMethod, $id);
         $event->process_request();
         break;
     case ATHLETE_MODULE:
-        // s
         $athlete = new Controller(new Athlete($db), $user, $requestMethod, $id);
         $athlete->process_request();
         break;
     default:
-        header(HTTP_NOT_FOUND);
+        header(Controller::HTTP_NOT_FOUND);
         exit();
 }
 /*
