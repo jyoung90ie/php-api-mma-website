cd628b899907ae8d086d887999f90adbae6b2e88
40030361 <jyoung22@qub.ac.uk>
2021-04-22 16:11:05 +0100

    Builds central controller for website
    The root index file will now be responsible for including templates based on URI query string.
    
    This makes it easier to maintain templates and logic.

--- index.php
+++ index.php
@@ -1,6 +1,35 @@
 <?php
+require_once 'autoload.php';
+
+require_once 'templates/header.php';
+
+$templatesFolder = 'templates/';
+
+if (!isset($_GET['page'])) {
+    $page = '';
+} else {
+    $page = $_GET['page'];
+}
+
+switch ($page) {
+    case 'athletes':
+        include_once $templatesFolder . 'athletes.php';
+        break;
+    case 'events':
+        include_once $templatesFolder . 'events.php';
+        break;
+    case 'event':
+        include_once $templatesFolder . 'event.php';
+        break;
+    case 'rankings':
+        include_once $templatesFolder . 'rankings.php';
+        break;
+    default:
+        include_once $templatesFolder . 'index.php';
+        break;
+
+}
 
 
-
-
+require_once 'templates/footer.php';
 ?>
