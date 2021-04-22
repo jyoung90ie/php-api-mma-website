069d0ee93d45c1bf11768f24f143498fc02829b8
40030361 <jyoung22@qub.ac.uk>
2021-04-22 15:09:34 +0100

    Adds new APIRequest object for making API requests and processing responses.
    All data will be sent and received through the API, this object acts as the facilitator for all API engagement.
    
    Associated testing has been added.

---
+++ helpers/APIRequest.php
@@ -0,0 +1,171 @@
+<?php
+
+
+namespace helpers;
+
+
+class APIRequest
+{
+    const API_KEY = 'test123';
+
+    private string $apiBaseUrl;
+    private string $apiModule;
+    private ?int $itemId;
+    private ?array $queryStrings;
+    private ?array $apiResponse = null;
+
+    public function __construct(string $url, string $module, ?int $itemId = null, ?array $queryStrings = null)
+    {
+        $this->apiBaseUrl = rtrim($url, '/');
+        $this->apiModule = trim($module, '/');
+        $this->itemId = intvaL(ltrim($itemId, '/'));
+        $this->queryStrings = $queryStrings;
+    }
+
+    /**
+     * Retrieve data from api end point and store in instance var.
+     */
+    public function fetchApiData()
+    {
+        $apiDataUrl = $this->apiBaseUrl . '/' . $this->apiModule;
+
+        if (isset($this->itemId)) {
+            $apiDataUrl .= '/' . $this->itemId;
+        }
+
+        $apiDataUrl .= '?apiKey=' . self::API_KEY;
+        if (isset($this->queryStrings)) {
+            // remove any apiKey already set as part of querystring
+            if (isset($this->queryStrings['apiKey'])) {
+                unset($this->queryStrings['apiKey']);
+            }
+
+            $apiDataUrl .= '&' . http_build_query($this->queryStrings);
+        }
+
+        $apiRequest = curl_init();
+        curl_setopt($apiRequest, CURLOPT_URL, $apiDataUrl);
+        curl_setopt($apiRequest, CURLOPT_RETURNTRANSFER, true);
+
+        $apiResponse = curl_exec($apiRequest);
+        $metaResponse = curl_getinfo($apiRequest);
+
+        $httpCode = $metaResponse['http_code'];
+        $response = [];
+
+        if ($httpCode == 200 || $httpCode == 201 || $httpCode == 204) {
+            // successful GET (200) or successful POST (201)
+            $response = json_decode($apiResponse, true);
+        } else if ($httpCode == 400) {
+            // not authenticated
+            $response['Error'] = 'Bad request';
+
+        } else if ($httpCode == 401) {
+            // not authenticated
+            $response['Error'] = 'Not authenticated';
+        } else if ($httpCode == 403) {
+            // no permission to access
+            $response['Error'] = 'No permission';
+        } else if ($httpCode == 404) {
+            // not found
+            if (isset($this->queryStrings)) {
+                // try again without query strings
+                $this->queryStrings = null;
+                return $this->fetchApiData();
+            }
+            // definitely doesn't exist
+            $response['Error'] = 'Page not found';
+
+        } else {
+            // something unexpected
+            $response['Error'] = 'Unexpected error';
+        }
+
+        curl_close($apiRequest);
+
+        $this->apiResponse = $response;
+        return $this->apiResponse;
+    }
+
+
+    /**
+     * Generates HTML for paginated results so that only a limited amount of information is shown on each page. This
+     * also limits query impact.
+     *
+     * @return false|string HTML string if result is paginated, otherwise will return false.
+     */
+    function displayPagination()
+    {
+        $maxPaginationPages = 5;
+
+        if (!isset($this->apiResponse) || !isset($this->apiResponse['links'])) {
+            return false;
+        }
+
+        $links = $this->apiResponse['links'];
+        parse_str(parse_url($links['self'], PHP_URL_QUERY), $currentPageResults);
+        $totalResults = $this->apiResponse['totalResults'] ?? null;
+
+        $startingResult = intval($currentPageResults['start']);
+        $resultsPerPage = $this->apiResponse['resultsPerPage'] ?? null;
+
+        $totalPages = intval(ceil($totalResults / $resultsPerPage) ?? 1);
+        $activePage = intval(floor($startingResult / $resultsPerPage) + 1);
+
+
+        $outputHTML = '';
+        $prevLink = '';
+        $prevLinkClass = ' disabled';
+        $nextLink = '';
+        $nextLinkClass = ' disabled';
+
+
+        // previous button
+        if ($activePage > 1) {
+            $prevLink = '?' . http_build_query(['start' => $startingResult - $resultsPerPage]);
+            $prevLinkClass = '';
+        }
+
+
+        $pageDifferential = intval($maxPaginationPages / 2);
+
+        $firstPage = $activePage - $pageDifferential;
+        $firstPage = ($firstPage < 1 ? 1 : $firstPage);
+
+        $lastPage = $activePage + $pageDifferential;
+        $lastPage = ($lastPage > $totalPages ? $totalPages : $lastPage);
+
+        $outputHTML .= '              <li class="page-item' . $prevLinkClass . '">
+                        <a class="page-link" href="' . $prevLink . '">Previous</a>
+                    </li>';
+
+        for ($page = $firstPage; $page <= $lastPage; $page++) {
+            $urlStartValue = $startingResult + $resultsPerPage * ($page - $activePage);
+            $pageUrl = '?' . http_build_query(['start' => $urlStartValue]);
+
+            $activePageClass = '';
+
+            if ($activePage == $page) {
+                $activePageClass = ' active';
+            }
+
+            $outputHTML .= '              <li class="page-item' . $activePageClass . '">
+                        <a class="page-link" href="' . $pageUrl . '">' . $page . '</a>
+                        </li>';
+        }
+
+        // next button
+        if ($activePage < $totalPages) {
+            $nextLink = '?' . http_build_query(['start' => $startingResult + $resultsPerPage]);
+            $nextLinkClass = '';
+        }
+
+        $outputHTML .= '              <li class="page-item ' . $nextLinkClass . '">
+                        <a class="page-link" href="' . $nextLink . '">Next</a>
+                    </li>';
+
+
+        return $outputHTML;
+    }
+
+}
---
+++ tests/helpers/APIRequestTest.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace helpers;
+
+require_once '../../autoload.php';
+
+use PHPUnit\Framework\TestCase;
+
+class APIRequestTest extends TestCase
+{
+    private string $apiUrl;
+    private string $eventEndpoint;
+    private string $athleteEndpoint;
+    private string $fightEndpoint;
+    private string $userEndpoint;
+    private APIRequest $apiRequest;
+
+
+    protected function setUp(): void
+    {
+        $this->apiUrl = "http://localhost:8888/promma/api";
+        $this->eventEndpoint = "/event";
+        $this->athleteEndpoint = "/athlete";
+        $this->fightEndpoint = "/fight";
+        $this->userEndpoint = "/user";
+    }
+
+    public function testFetchResponseEventAllValid(): void
+    {
+        $apiModule = $this->eventEndpoint;
+        $apiRequest = new APIRequest($this->apiUrl, $apiModule);
+
+        $response = $apiRequest->fetchApiData();
+
+        $apiRequest->displayPagination();
+
+        self::assertFalse(isset($response['Error']));
+
+        self::assertTrue(isset($response['totalResults']));
+        self::assertTrue(isset($response['currentResults']));
+        self::assertTrue(isset($response['data']));
+    }
+
+    public function testFetchResponseEventOneValid(): void
+    {
+        $apiModule = $this->eventEndpoint;
+        $itemId = 1;
+        $apiRequest = new APIRequest($this->apiUrl, $apiModule, $itemId);
+
+        $response = $apiRequest->fetchApiData();
+
+        self::assertFalse(isset($response['Error']));
+
+        self::assertTrue(isset($response['EventID']));
+        self::assertTrue(isset($response['Fights']));
+    }
+
+    public function testFetchResponseEventOneInvalid(): void
+    {
+        $apiModule = "/events";
+        $itemId = 99999;
+        $apiRequest = new APIRequest($this->apiUrl, $apiModule, $itemId);
+
+        $response = $apiRequest->fetchApiData();
+
+        self::assertTrue(isset($response['Error']));
+        self::assertFalse(isset($response['EventID']));
+
+        $apiModule = $this->eventEndpoint;
+        $itemId = -1;
+        $apiRequest = new APIRequest($this->apiUrl, $apiModule, $itemId);
+
+        $response = $apiRequest->fetchApiData();
+
+        var_dump($response);
+
+        self::assertTrue(isset($response['Error']));
+        self::assertFalse(isset($response['EventID']));
+
+
+        $itemId = "invalid";
+
+        // expect itemId to through typeError exception as it's not an int
+         $this->expectException(\TypeError::class);
+        $apiRequest = new APIRequest($this->apiUrl, $apiModule, $itemId);
+    }
+
+    // athletes
+    public function testFetchResponseAthleteAllValid(): void
+    {
+        $apiModule = $this->athleteEndpoint;
+        $apiRequest = new APIRequest($this->apiUrl, $apiModule);
+
+        $response = $apiRequest->fetchApiData();
+
+        self::assertFalse(isset($response['Error']));
+
+        self::assertTrue(isset($response['totalResults']));
+        self::assertTrue(isset($response['currentResults']));
+        self::assertTrue(isset($response['data']));
+    }
+
+    public function testFetchResponseAthleteOneValid(): void
+    {
+        $apiModule = $this->athleteEndpoint;
+        $itemId = 1;
+        $apiRequest = new APIRequest($this->apiUrl, $apiModule, $itemId);
+
+        $response = $apiRequest->fetchApiData();
+
+        self::assertFalse(isset($response['Error']));
+
+        self::assertTrue(isset($response['AthleteID']));
+        self::assertTrue(isset($response['Fights']));
+    }
+
+    public function testFetchResponseAthleteOneInvalid(): void
+    {
+        $apiModule = "/athletes";
+        $itemId = 99999;
+        $apiRequest = new APIRequest($this->apiUrl, $apiModule, $itemId);
+
+        $response = $apiRequest->fetchApiData();
+
+        self::assertTrue(isset($response['Error']));
+        self::assertFalse(isset($response['EventID']));
+
+        $apiModule = $this->athleteEndpoint;
+        $itemId = -1;
+        $apiRequest = new APIRequest($this->apiUrl, $apiModule, $itemId);
+
+        $response = $apiRequest->fetchApiData();
+
+        self::assertTrue(isset($response['Error']));
+        self::assertFalse(isset($response['EventID']));
+
+
+        $itemId = "invalid";
+
+        // expect itemId to through typeError exception as it's not an int
+         $this->expectException(\TypeError::class);
+        $apiRequest = new APIRequest($this->apiUrl, $apiModule, $itemId);
+    }
+
+}
