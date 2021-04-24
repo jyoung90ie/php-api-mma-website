65ee141f4ef2b709bd0241d67c0c302559cb90dc
40030361 <jyoung22@qub.ac.uk>
2021-04-24 19:16:44 +0100

    Extracts event template into TemplateHelper class

--- templates/events.php
+++ templates/events.php
@@ -1,6 +1,10 @@
 <?php
 
+namespace templates;
+
 use helpers\APIRequest;
+use helpers\TemplatesHelper;
+
 
 if (!constant("API_URL")) {
     echo 'Api address not set';
@@ -29,49 +33,7 @@ $events = $results['data'];
     <hr>
     <?php
     foreach ($events as $event) {
-        // generate athlete images
-        $athletes = $event['Headliners'];
-        unset($athleteOneImage, $athleteTwoImage);
-
-        // variables for rendering in template
-        $athleteOne = $athletes[0];
-        $athleteTwo = $athletes[1];
-
-        $athleteOneSurname = explode(" ", $athleteOne['AthleteName']);
-        $athleteOneSurname = $athleteOneSurname[sizeof($athleteOneSurname) - 1];
-
-        $athleteTwoSurname = explode(" ", $athleteTwo['AthleteName']);
-        $athleteTwoSurname = $athleteTwoSurname[sizeof($athleteTwoSurname) - 1];
-
-        $eventHeadliner = $athleteOneSurname . ' vs ' . $athleteTwoSurname;
-        $eventHeadliner = strtoupper($eventHeadliner);
-
-        $eventUrl = '?page=event&id=' . $event['EventID'];
-
-        ?>
-        <!-- Event -->
-        <div class="event row" onclick="window.location='<?= $eventUrl ?>'">
-            <div class="col-12 col-md-2 name">
-                Pro MMA <?= sprintf('%03d', $event['EventID']) ?>
-            </div>
-            <div class="col-12 col-md-4 athlete-images">
-                <div class="athlete-left">
-                    <img src="<?= $athleteOne['AthleteImage'] ?>" alt="<?= $athleteOne['AthleteName'] ?>"/>
-                </div>
-                <div class="athlete-right">
-                    <img src="<?= $athleteTwo['AthleteImage'] ?>" alt="<?= $athleteTwo['AthleteName'] ?>"/>
-                </div>
-            </div>
-            <div class="col-12 col-md-6">
-                <span class="headliner">
-                    <a href="<?= $eventUrl ?>"><?= $eventHeadliner ?></a>
-                </span>
-                <span class="date"><?= DateTime::createFromFormat('Y-m-d', $event['EventDate'])->format('D, d F Y, h:i A T') ?></span>
-                <span class="location"><?= $event['EventLocation'] ?></span>
-            </div>
-        </div>
-        <!-- ./Event -->
-        <?php
+        TemplatesHelper::displayEvent($event);
     }
     ?>
 
