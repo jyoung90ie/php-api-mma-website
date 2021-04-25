d680ff41734367816fb8544e26cb95e716b9dc0e
40030361 <jyoung22@qub.ac.uk>
2021-04-25 15:31:19 +0100

    Adds ability to dynamically produce fight stats and outcome badges

--- helpers/TemplatesHelper.php
+++ helpers/TemplatesHelper.php
@@ -66,6 +66,69 @@ class TemplatesHelper
         <?php
     }
 
+    /**
+     * @param array $fightStats
+     */
+    static function displayFightComparisonData(array $fightStats, string $redAthlete, string $blueAthlete)
+    {
+        if (!isset($fightStats) || !isset($redAthlete) || !isset($blueAthlete)) {
+            return;
+        }
+
+        foreach ($fightStats as $fightStat) {
+            if (sizeof($fightStats) > 0) {
+                $type = $fightStat['type'] ?? '';
+                $redStats = $fightStat[$redAthlete];
+                $blueStats = $fightStat[$blueAthlete];
+
+                // check if stats contain thrown and landed, if not, changed output
+                if (isset($redStats['landed']) && isset($redStats['thrown'])) {
+                    $redThrown = $redStats['thrown'] ?? 0;
+                    $redLanded = $redStats['landed'] ?? 0;
+
+                    $blueThrown = $blueStats['thrown'] ?? 0;
+                    $blueLanded = $blueStats['landed'] ?? 0;
+
+                    $redThrownPercent = ($redThrown > 0 ? ($redLanded / $redThrown) * 100 : 0);
+                    $blueThrownPercent = ($blueThrown > 0 ? ($blueLanded / $blueThrown) * 100 : 0);
+
+                    $redThrownText = sprintf('%d%% of %d', $redThrownPercent, $redThrown) ?? '';
+                    $blueThrownText = sprintf('%d%% of %d', $blueThrownPercent, $blueThrown) ?? '';
+                } else {
+                    // does not contain thrown and landed data points
+                    $redThrownText = '';
+                    $blueThrownText = '';
+
+                    // use reset() to get first element
+                    $redLanded = reset($redStats);
+                    $blueLanded = reset($blueStats);
+                }
+
+
+
+                ?>
+                <div class="fight-stats row">
+                    <div class="col-4">
+                        <span class="total-landed red"><?= $redLanded ?></span>
+                        <span class="total-thrown"><?= $redThrownText ?></span>
+                    </div>
+                    <div class="col-4 charts">
+                        <div class="chart">
+
+                        </div>
+                        <span class="chart-text"><?= $type ?></span>
+                    </div>
+                    <div class="col-4">
+                        <span class="total-landed blue"><?= $blueLanded ?></span>
+                        <span class="total-thrown"><?= $blueThrownText ?></span>
+                    </div>
+                </div>
+                <?php
+            }
+
+        }
+    }
+
 
     /**
      * Generates links for the navbar - highlighting the active page.
@@ -74,7 +137,7 @@ class TemplatesHelper
      * @param string $activePage the active page in the format '?page=pageName'
      * @return string generated HTML for navbar
      */
-    static function genNavbar(array $navbarPages, string $activePage): string
+    static function displayNavBar(array $navbarPages, string $activePage): string
     {
         $outputHTML = '';
         foreach ($navbarPages as $page) {
@@ -91,6 +154,28 @@ class TemplatesHelper
 
         return $outputHTML;
     }
+
+    /**
+     * Creates HTML for indicating the winner of a fight
+     *
+     * @param $athlete array of athlete data
+     * @param int|null $winnerId athleteId for the winner
+     * @return string of HTML
+     */
+    static function outcomeBadge(array $athlete, ?int $winnerId): string
+    {
+        if (is_null($winnerId)) {
+            $outputHTML = '<span class="fight-outcome-badge draw">draw</span>';
+        } else if ($winnerId == $athlete['AthleteID']) {
+            $outputHTML = '<span class="fight-outcome-badge win">win</span>';
+        } else {
+            $outputHTML = '<span class="fight-outcome-badge"></span>';
+        }
+
+        return $outputHTML;
+    }
 }
+
+
 
 ?>
