86c35b9ae3916a104795199687562a5ba56313ac
40030361 <jyoung22@qub.ac.uk>
2021-04-25 15:32:21 +0100

    Updates template to display data from database

--- templates/fight.php
+++ templates/fight.php
@@ -1,6 +1,7 @@
 <?php
 
 use helpers\APIRequest;
+use helpers\TemplatesHelper;
 
 // no/invalid id - redirect
 if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
@@ -16,7 +17,6 @@ if (!constant("API_URL")) {
 $apiModule = "/fight";
 $id = intval($_GET['id']);
 
-
 $apiRequest = new APIRequest(constant("API_URL"), $apiModule, $id, $queryString);
 $results = $apiRequest->fetchApiData();
 
@@ -25,43 +25,127 @@ if (isset($results['Error']) || !$results) {
     return;
 }
 
+$athletes = $results['Athletes'];
+
+if (sizeof($athletes) == 0) {
+    echo '<p>No fight data</p>';
+}
+
+$statsData = processApiResult($athletes);
+
+$winnerId = $results['WinnerAthleteID'];
+
+$athleteRed = $athletes[0];
+$athleteBlue = $athletes[1];
+
+$athleteNameRed = $athleteRed['AthleteName'];
+$athleteNameBlue = $athleteBlue['AthleteName'];
+
+$finishRound = $results['WinRound'] ?? 'TBC';
+$finishTime = $results['WinRoundTime'] ?? 'TBC';
+$titleBout = ($results['TitleBout'] == 1 ? 'Yes' : 'No');
+$outcome = $results['Outcome'] ?? 'TBC';
+
+$eventUrl = '?page=event&id=' . $results['EventID'];
+
 ?>
 
-<main class="fight-detail-container container">
-    <?php
-    if (isset($_SERVER['HTTP_REFERER'])) {
-        ?>
-        <div>
-            <a class="btn btn-more" href="<?= $_SERVER['HTTP_REFERER'] ?>">Back to previous page</a>
+    <main class="fight-detail-container container">
+        <a class="btn btn-more" href="<?= $eventUrl ?>">Back to Event</a>
+        <h2>Fight Breakdown</h2>
+        <div class="fight-outcome row">
+            <div class="col-6 col-md-3">
+                <span>Round</span>
+                <span><?= $finishRound ?></span>
+            </div>
+            <div class="col-6 col-md-3">
+                <span>Time</span>
+                <span><?= $finishTime ?></span>
+            </div>
+            <div class="col-6 col-md-3">
+                <span>Outcome</span>
+                <span><?= $outcome ?></span>
+            </div>
+            <div class="col-6 col-md-3">
+                <span>Title Bout?</span>
+                <span><?= $titleBout ?></span>
+            </div>
         </div>
-        <?php
+        <div class="fight-athletes row">
+            <div class="col-6">
+                <span class="athlete-name"><?= $athleteNameRed ?></span>
+                <?= TemplatesHelper::outcomeBadge($athleteRed, $winnerId) ?>
+            </div>
+            <div class="col-6">
+                <span class="athlete-name"><?= $athleteNameBlue ?></span>
+                <?= TemplatesHelper::outcomeBadge($athleteBlue, $winnerId) ?>
+            </div>
+        </div>
+
+        <?php TemplatesHelper::displayFightComparisonData($statsData, $athleteNameRed, $athleteNameBlue); ?>
+    </main>
+
+
+<?php
+
+/**
+ * Generates an array of fight data which can be used by TemplatesHelper::displayFightComparisonData.
+ *
+ * Array format:
+ * [
+ *      ['Data Point Name'] =>
+ *      {
+ *          'type' => 'Data Point Name',
+ *          'athleteRed' => { 'landed' => int, 'thrown' => int},
+ *          'athleteRed' => { 'landed' => int, 'thrown' => int}
+ *      },
+ *      [...] => { ... }
+ * ]
+ *
+ * @param array $athletesData data from fight detail API endpoint
+ * @return array|void
+ */
+function processApiResult(array $athletesData)
+{
+    $statDisplayNames = [
+        'stats_strikesLanded' => 'Strikes',
+        'stats_strikesThrown' => 'Strikes',
+        'stats_significantStrikesLanded' => 'Significant Strikes',
+        'stats_significantStrikesThrown' => 'Significant Strikes',
+        'stats_takedownsLanded' => 'Takedowns',
+        'stats_takedownsThrown' => 'Takedowns',
+        'stats_positionReversals' => 'Reversals',
+        'stats_knockDowns' => 'Knockdowns',
+        'stats_submissionAttempts' => 'Submission Attempts'
+    ];
+
+    $statData = [];
+
+    if (sizeof($athletesData) !== 2) {
+        echo 'Must have two athletes';
+        return;
     }
-    ?>
-    <h2>Fight Breakdown</h2>
-    <div class="fight-athletes row">
-        <div class="col-6">Fighter 1</div>
-        <div class="col-6">Fighter 2</div>
-    </div>
-    <div class="fight-outcome row">
-        <div class="col-4">
-            <span>Round</span>
-            <span>5</span>
-        </div>
-        <div class="col-4">
-            <span>Time</span>
-            <span>5:00</span>
-        </div>
-        <div class="col-4">
-            <span>Method</span>
-            <span>DECISION - UNANIMOUS</span>
-        </div>
-    </div>
-    <div class="fight-breakdown row">
-        <div class="col-12 col-md-6">
 
-        </div>
-        <div class="col-12 col-md-6">
 
-        </div>
-    </div>
-</main>
+    $athleteRed = $athletesData[0]['AthleteName'];
+    $athleteBlue = $athletesData[1]['AthleteName'];
+
+    foreach ($statDisplayNames as $key => $value) {
+        if (!isset($statData[$value][$athleteRed][$key])) {
+            $statData[$value]['type'] = $value;
+
+            if (stripos($key, 'landed') !== false) {
+                $keyDisplay = 'landed';
+            } else if (stripos($key, 'thrown') !== false) {
+                $keyDisplay = 'thrown';
+            } else {
+                $keyDisplay = $key;
+            }
+
+            $statData[$value][$athleteRed][$keyDisplay] = $athletesData[0][$key];
+            $statData[$value][$athleteBlue][$keyDisplay] = $athletesData[1][$key];
+        }
+    }
+
+    return $statData;
+}
