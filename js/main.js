7170af859724ea238b0157dab5b7d6175283d2c4
40030361 <jyoung22@qub.ac.uk>
2021-04-27 10:42:12 +0100

    Adds import for chart.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+// import chart functionality
+import Chart from 'chart.js/auto';
+
 // attach event listener to tab navigation for rankings
 $('#athleteRankings a').on('click', function (e) {
     e.preventDefault();
