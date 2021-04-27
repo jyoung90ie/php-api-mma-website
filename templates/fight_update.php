1bc61ddae00da4981138da29e3fa931a1c27deab
40030361 <jyoung22@qub.ac.uk>
2021-04-27 19:40:23 +0100

    Adds page to update fights

--- templates/fight_update.php
+++ templates/fight_update.php
@@ -3,15 +3,14 @@
 use helpers\APIRequest;
 
 $permissionModule = \models\Fight::PERMISSION_AREA;
-$permissionType = 'CREATE';
+$permissionType = 'UPDATE';
+$apiModule = 'fight';
 
 \helpers\HelperFunctions::checkPermission($permissionModule, $permissionType);
 
-$apiEndPoint = API_URL . '/fight?apiKey=' . API_KEY;
-
 
 // no/invalid id - redirect
-if (!isset($_GET['eventid']) || !is_numeric($_GET['eventid'])) {
+if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
     header("Location: ?page=events");
 }
 
@@ -20,8 +19,16 @@ if (!constant("API_URL")) {
     return;
 }
 
-$id = intval($_GET['eventid']);
+$id = intval($_GET['id']);
 
+$apiEndPoint = API_URL . '/' . $apiModule . '/' . $id . '?apiKey=' . API_KEY;
+
+// get existing values for event - calling early so can store fightAthleteIDs
+$apiRequest = new APIRequest(API_URL, 'fight', API_KEY, $id, $queryString);
+$results = $apiRequest->fetchApiData();
+
+$fightAthleteId1 = $results['Athletes'][0]['FightAthleteID'];
+$fightAthleteId2 = $results['Athletes'][1]['FightAthleteID'];
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $fields = [
@@ -35,13 +42,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         }
     }
 
-    // make sure the same fighter hasn't been selected twice
+// make sure the same fighter hasn't been selected twice
     if (!empty($_POST['AthleteID1']) && $_POST['AthleteID1'] == $_POST['AthleteID2']) {
         $validationErrors['AthleteID1'] = 'This athlete has been selected twice - please select another athlete';
         $validationErrors['AthleteID2'] = $validationErrors['AthleteID1'];
     }
 
-    // api processing
+// api processing
     if (sizeof($validationErrors) == 0) {
         // convert form data into array
         $formContents = file_get_contents("php://input");
@@ -49,30 +56,34 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
         // transform form contents
         $formContents['TitleBout'] = $formContents['TitleBout'] == 'yes' ? 1 : 0;
+        $formContents['FightAthleteID1'] = $fightAthleteId1;
+        $formContents['FightAthleteID2'] = $fightAthleteId2;
 
         // add default role
         $jsonContents = json_encode($formContents);
 
         $apiRequest = curl_init();
         curl_setopt($apiRequest, CURLOPT_URL, $apiEndPoint);
-        curl_setopt($apiRequest, CURLOPT_POST, true);
+        curl_setopt($apiRequest, CURLOPT_CUSTOMREQUEST, 'PUT');
         curl_setopt($apiRequest, CURLOPT_POSTFIELDS, $jsonContents);
         curl_setopt($apiRequest, CURLOPT_RETURNTRANSFER, true);
 
         $apiResponse = json_decode(curl_exec($apiRequest), true);
         $metaResponse = curl_getinfo($apiRequest);
 
-        // if created, header will return 201
-        if (isset($metaResponse['http_code']) && $metaResponse['http_code'] == 201) {
+        // if updated, header will return 204
+        if (isset($metaResponse['http_code']) && $metaResponse['http_code'] == 204) {
 
             unset($apiResponse); // api will return number of records created when successfully
+            unset($queryString['action']); // remove so that redirect takes user to event page
 
-            $userNotification = 'Fight has been created and added to the Event';
+            $userNotification = 'Fight updated';
             \helpers\HelperFunctions::addNotification($userNotification);
 
-            header("Location: ?page=event&id=$id");
-        }
+            $redirect = '?' . http_build_query($queryString);
 
+            header("Location: $redirect");
+        }
     }
 }
 
@@ -87,11 +98,17 @@ $weights = $weightRequest->fetchApiData();
 $athleteRequest = new APIRequest(API_URL, 'athlete', API_KEY, null, ['limit' => 5000, 'limitOverride' => true]);
 $athletes = $athleteRequest->fetchApiData();
 
+if (isset($results['Error'])) {
+    if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+        // get request means no data has been submitted yet.. so error is a result of non-existent record
+        header('Location: ?page=events');
+    }
+}
 
 ?>
 
 <main class="container">
-    <h2>Event - Add Fight</h2>
+    <h2>Event - Update Fight</h2>
 
     <?= \helpers\HelperFunctions::displayApiError($apiResponse ?? []); ?>
     <form action="" method="post">
@@ -100,7 +117,7 @@ $athletes = $athleteRequest->fetchApiData();
                 <label for="EventID" class="col-form-label">Event ID</label>
             </div>
             <div class="col-auto">
-                <input type="text" name="EventID" class="form-control" value="<?= $id ?>" readonly>
+                <input type="text" name="EventID" class="form-control" value="<?= $results['EventID'] ?>" readonly>
             </div>
         </div>
 
@@ -113,8 +130,9 @@ $athletes = $athleteRequest->fetchApiData();
                     <option value="" selected></option>
                     <?php
                     if (isset($referees['data'])) {
+                        $refId = $_POST['RefereeID'] ?? $results['RefereeID'];
                         foreach ($referees['data'] as $ref) {
-                            $selected = ($_POST['RefereeID'] == $ref['RefereeID'] ? ' selected' : '');
+                            $selected = ($refId == $ref['RefereeID'] ? ' selected' : '');
                             echo '<option value="' . $ref['RefereeID'] . '"' . $selected . '>';
                             echo $ref['RefereeName'];
                             echo '</option>';
@@ -136,8 +154,11 @@ $athletes = $athleteRequest->fetchApiData();
             </div>
             <div class="col-auto">
                 <select name="TitleBout" class="form-select" aria-label="Is this a title fight?">
-                    <option value="no" selected>No</option>
-                    <option value="yes">Yes</option>
+                    <option value="no" <?= ($_POST['TitleBout'] ?? $results['TitleBout']) == 0 ? 'selected' : '' ?>>No
+                    </option>
+                    <option value="yes" <?= ($_POST['TitleBout'] ?? $results['TitleBout']) == 1 ? 'selected' : '' ?>>
+                        Yes
+                    </option>
                 </select>
             </div>
             <div class="col-auto">
@@ -153,8 +174,12 @@ $athletes = $athleteRequest->fetchApiData();
             </div>
             <div class="col-auto">
                 <select name="NumOfRounds" class="form-select" aria-label="Select the number of rounds">
-                    <option value="3" selected>3</option>
-                    <option value="5">5</option>
+                    <option value="3" <?= ($_POST['NumOfRounds'] ?? $results['NumOfRounds']) == 3 ? 'selected' : '' ?>>
+                        3
+                    </option>
+                    <option value="5" <?= ($_POST['NumOfRounds'] ?? $results['NumOfRounds']) == 5 ? 'selected' : '' ?>>
+                        5
+                    </option>
                 </select>
             </div>
             <div class="col-auto">
@@ -173,8 +198,9 @@ $athletes = $athleteRequest->fetchApiData();
                     <option value="" selected></option>
                     <?php
                     if (isset($weights['data'])) {
+                        $weightId = $_POST['WeightClassID'] ?? $results['WeightClassID'];
                         foreach ($weights['data'] as $weight) {
-                            $selected = ($_POST['WeightClassID'] == $weight['WeightClassID'] ? ' selected' : '');
+                            $selected = ($weightId == $weight['WeightClassID'] ? ' selected' : '');
                             echo '<option value="' . $weight['WeightClassID'] . '"' . $selected . '>';
                             echo $weight['WeightClass'];
                             echo '</option>';
@@ -199,8 +225,9 @@ $athletes = $athleteRequest->fetchApiData();
                     <option value="" selected></option>
                     <?php
                     if (isset($athletes['data'])) {
+                        $athleteId1 = $_POST['AthleteID1'] ?? $results['Athletes'][0]['AthleteID'] ?? '';
                         foreach ($athletes['data'] as $athlete) {
-                            $selected = ($_POST['AthleteID1'] == $athlete['AthleteID'] ? ' selected' : '');
+                            $selected = ($athleteId1 == $athlete['AthleteID'] ? ' selected' : '');
                             echo '<option value="' . $athlete['AthleteID'] . '"' . $selected . '>';
                             echo $athlete['AthleteName'];
                             echo '</option>';
@@ -225,8 +252,9 @@ $athletes = $athleteRequest->fetchApiData();
                     <option value="" selected></option>
                     <?php
                     if (isset($athletes['data'])) {
+                        $athleteId2 = $_POST['AthleteID2'] ?? $results['Athletes'][1]['AthleteID'] ?? '';
                         foreach ($athletes['data'] as $athlete) {
-                            $selected = ($_POST['AthleteID2'] == $athlete['AthleteID'] ? ' selected' : '');
+                            $selected = ($athleteId2 == $athlete['AthleteID'] ? ' selected' : '');
                             echo '<option value="' . $athlete['AthleteID'] . '"' . $selected . '>';
                             echo $athlete['AthleteName'];
                             echo '</option>';
@@ -242,6 +270,6 @@ $athletes = $athleteRequest->fetchApiData();
             </div>
         </div>
 
-        <button type="submit" class="btn btn-primary">Create</button>
+        <button type="submit" class="btn btn-primary">Update</button>
     </form>
 </main>
