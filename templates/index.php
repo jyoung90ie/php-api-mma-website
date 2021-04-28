bf6f403138450fdaba9a5d553933cd8b77da29cd
40030361 <jyoung22@qub.ac.uk>
2021-04-28 20:20:05 +0100

    Implements featured athlete

--- templates/index.php
+++ templates/index.php
@@ -12,7 +12,6 @@ if (!constant("API_URL")) {
 }
 
 
-
 // get 2 most recent events
 $apiModule = "/event";
 $queryString['start'] = 0;
@@ -28,60 +27,55 @@ if (isset($results['Error']) || !$results) {
 
 $events = $results['data'];
 
+$apiModule = 'athlete';
+$queryString['random'] = 1; // needed to invoke getRandom() function
+
+$apiRequest = new APIRequest(API_URL, $apiModule, API_KEY, null, $queryString);
+$apiResponse = $apiRequest->fetchApiData();
+$athleteData = $apiResponse['data'];
+
 ?>
 
 <main class="content container-fluid">
-    <!-- NewsContainer -->
-    <div class="news container">
-        <h2><i class="far fa-newspaper"></i> Latest News</h2>
-        <div class="row">
-            <!-- News1 -->
-            <div class="card mb-3 col-lg-5">
-                <div class="row no-gutters">
-                    <div class="col-md-4 my-card-img">
-                        <img src="images/news1.jpg" class="card-img" alt="...">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Fighter Retires</h5>
-                            <p class="card-text">After wide speculation it has been confirmed that Fighter McFighter
-                                has retired...</p>
-                            <p class="card-text"><small class="text-muted">Last updated 6 hours ago</small></p>
+    <!-- FeaturedAthlete -->
+    <div class="featured-athletes container">
+        <h2><i class="fas fa-users"></i> Featured Athletes</h2>
+        <div class="row justify-content-evenly">
+            <?php
+            foreach ($athleteData as $athlete) {
+                $athleteUrl = '?page=athlete&id=' . $athlete['AthleteID'];
+                ?>
+                <!-- Athlete -->
+                <div class="card mb-3 col-lg-3 m-2" onclick="window.location='<?= $athleteUrl ?>'">
+                    <span class="athlete-name"><?= $athlete['AthleteName'] ?></span>
+                    <img src="<?= $athlete['AthleteImage'] ?>" class="card-img" alt="Headshot image">
+                    <div class="d-flex flex-row justify-content-evenly athlete-stats">
+                        <div class="flex-column">
+                            <div>Fights</div>
+                            <div><?= $athlete['TotalFights'] ?></div>
+                        </div>
+                        <div class="flex-column">
+                            <div>Wins</div>
+                            <div><?= $athlete['TotalWins'] ?></div>
+                        </div>
+                        <div class="flex-column">
+                            <div>Submissions</div>
+                            <div><?= $athlete['TotalSubmissions'] ?></div>
                         </div>
                     </div>
+
                 </div>
-            </div>
-            <!-- ./News1 -->
-            <!-- News2 -->
-            <div class="card mb-3 offset-lg-1 col-lg-5">
-                <div class="row no-gutters">
-                    <div class="col-md-4 my-card-img">
-                        <img src="images/news2.jpg" class="card-img" alt="...">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Pro MMA 125 Date Confirmed</h5>
-                            <p class="card-text">
-                                Today it has been confirmed that Fighter A will face Fighter B for the belt on
-                                24 August 2021. <br /> <br />The main card is still shaping up with news expected in
-                                the
-                                coming
-                                weeks.
-                            </p>
-                            <p class="card-text"><small class="text-muted">Last updated 2 days ago</small></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- ./News2 -->
+                <!-- ./Athlete -->
+                <?php
+            }
+            ?>
         </div>
-        <!-- ./Horizontal Card -->
     </div>
-    <!-- ./NewsContainer -->
+    <!-- ./FeaturedAthletes -->
 
     <!-- EventsContainer -->
     <div class="home-events-container">
-        <h2><i class="fas fa-calendar-alt"></i> Upcoming Events</h2>
+        <h2><i class="fas fa-calendar-alt"></i> Events</h2>
         <div class="container">
             <!-- Events -->
             <?php
